d93af1f51d90a5fff5a2446a8f19122d8fb95f86
FA CREATIVE <facreative.apps@gmail.com>
2023-05-11 16:49:37 +0600

    test

--- Component/Home/Home.js
+++ Component/Home/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Home = () => {
     return (
         <View>
-            <Text style={styles.title}> HOME PAGE 222 </Text>
+            <View>
+                <Text style={styles.title}> Main Section </Text>
+                <Text style={styles.paragraph}> First, you will need to start Metro, the JavaScript bundler that ships with React Native. Metro "takes in an entry file and various options, and returns a single JavaScript file that includes all your code and its dependencies."—Metro Docs
+
+                    To start Metro, run npx react-native start inside your React Native project folder:
+
+                </Text>
+                <Image source={{ uri: 'https://media.springernature.com/full/springer-cms/rest/v1/img/23611344/v1/height/320' }}
+                    style={{ width: 400, height: 400 }} />
+
+            </View>
+            <View>
+                <Text style={styles.title}> Secondary Section </Text>
+                <Text style={styles.paragraph}>Now that you have successfully run the app, let's modify it.
+
+                    Open App.tsx in your text editor of choice and edit some lines.
+                    Press the R key twice or select Reload from the Developer Menu (Ctrl + M) to see your changes! </Text>
+            </View>
         </View>
     )
 };
@@ -13,7 +30,12 @@ export default Home;
 const styles = StyleSheet.create({
     title: {
         color: "blue",
-        fontSize: 30,
+        fontSize: 20,
         fontWeight: "bold"
+    },
+    paragraph: {
+        fontSize: 15,
+        opacity: 1,
+        color: "black"
     }
 })
